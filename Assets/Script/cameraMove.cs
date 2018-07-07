adc2773a5dbb944b969c486ba46affc9ec147834
软泥怪的大裤衩 <alienminawisa@outlook.com>
2018-07-08 05:15:11 +0800

    镜头效果

---
+++ Assets/Script/cameraMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour {
+    public tetrisMove tetris;
+    public GameObject water;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+    private void FixedUpdate()
+    {
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, tetris.topCube + 10, this.gameObject.transform.position.z);
+        if (water.transform.position.y < tetris.topCube - 5)
+        {
+            water.transform.position = new Vector3(water.transform.position.x, tetris.topCube - 5, water.transform.position.z);
+        }
+    }
+}
