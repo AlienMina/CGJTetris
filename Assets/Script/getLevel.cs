3dab1c253ed5379025d58ff49feb6469c8f8eab2
软泥怪的大裤衩 <alienminawisa@outlook.com>
2018-07-08 15:56:00 +0800

    UI

---
+++ Assets/Script/getLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getLevel : MonoBehaviour {
+
+    public tetrisMove spaceCube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.GetComponent<Text>().text = spaceCube.topCube.ToString();
+	}
+}
